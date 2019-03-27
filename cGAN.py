405ce0c62c6becef7064368af5d124e6692d06f6
siddsax <ss.siddharthasaxena@gmail.com>
2019-03-27 23:10:24 +0530

    changes

--- cGAN.py
+++ cGAN.py
@@ -18,15 +18,15 @@ if not os.path.exists('images'):
     os.makedirs('images')
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
+parser.add_argument('--n_epochs', type=int, default=500, help='number of epochs of training')
 parser.add_argument('--batch_size', type=int, default=64, help='size of the batches')
-parser.add_argument('--lr', type=float, default=0.0002, help='adam: learning rate')
+parser.add_argument('--lr', type=float, default=0.0003, help='adam: learning rate')
 parser.add_argument('--b1', type=float, default=0.5, help='adam: decay of first order momentum of gradient')
 parser.add_argument('--b2', type=float, default=0.999, help='adam: decay of first order momentum of gradient')
 parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
 parser.add_argument('--latent_dim', type=int, default=100, help='dimensionality of the latent space')
-parser.add_argument('--n_classes', type=int, default=10, help='number of classes for dataset')
-parser.add_argument('--img_size', type=int, default=32, help='size of each image dimension')
+parser.add_argument('--n_classes', type=int, default=7, help='number of classes for dataset')
+parser.add_argument('--img_size', type=int, default=256, help='size of each image dimension')
 parser.add_argument('--channels', type=int, default=1, help='number of image channels')
 parser.add_argument('--sample_interval', type=int, default=400, help='interval between image sampling')
 parser.add_argument('--dataPath', type=str, default="dataset", help='dataset path')
@@ -139,7 +139,7 @@ def sample_image(n_row, batches_done):
     labels = np.array([num for _ in range(n_row) for num in range(n_row)])
     labels = Variable(LongTensor(labels))
     gen_imgs = generator(z, labels)
-    save_image(gen_imgs.data, 'images/%d.png' % batches_done, nrow=n_row, normalize=True)
+    save_image(gen_imgs.data, 'images/n%d.png' % batches_done, nrow=n_row, normalize=True)
 
 # ----------
 #  Training
@@ -202,4 +202,4 @@ for epoch in range(opt.n_epochs):
 
         batches_done = epoch * len(dataloader) + i
         if batches_done % opt.sample_interval == 0:
-            sample_image(n_row=10, batches_done=batches_done)
+            sample_image(n_row=opt.n_classes, batches_done=batches_done)
